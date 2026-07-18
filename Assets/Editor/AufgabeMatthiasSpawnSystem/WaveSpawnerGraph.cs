6ba57d7734a30a0db747fd8ac316b1ea40d22e4f
LucaWScholz <lucawolfgangscholz@gmail.com>
2026-07-18 14:26:47 +0200

    Readme, Ui fix, Sound change, Map adjustment

--- Assets/Editor/AufgabeMatthiasSpawnSystem/WaveSpawnerGraph.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "New Wave Graph", menuName = "Wave Spawner")]
-public class WaveSpawnerGraph : ScriptableObject
-{
-    public List<WaveSpawnerNode> nodes = new List<WaveSpawnerNode>();
-    public string startNodeGuid;
-}
---
+++ README.txt
@@ -0,0 +1,3 @@
+Quellen:
+Sound:
+Turbine Thrust https://www.zapsplat.com/music/loop-turbines-of-a-bombardier-global-running-distant/ entnommen am 18.07.2026
